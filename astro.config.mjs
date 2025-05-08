e308636ee4de0a5c586624346a20e56d7e2caa23
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2025-05-09 00:10:21 +0200

    Remove PWA and add Coc to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
 import alpinejs from "@astrojs/alpinejs";
-import AstroPWA from "@vite-pwa/astro";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -20,45 +19,6 @@ export default defineConfig({
 		tailwind(),
 		sitemap(),
 		alpinejs(),
-		AstroPWA({
-			mode: "production",
-			base: "/",
-			scope: "/",
-			includeAssets: ["favicon.svg"],
-			registerType: "autoUpdate",
-			manifest: {
-				name: "PyLadiesCon 2025 - The global online PyLadies Conference",
-				short_name: "Astros",
-				theme_color: "#ffffff",
-				icons: [
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-			},
-			workbox: {
-				navigateFallback: "/404",
-				globPatterns: ["*.js"],
-			},
-			devOptions: {
-				enabled: false,
-				navigateFallbackAllowlist: [/^\/404$/],
-				suppressWarnings: true,
-			},
-		}),
 		icon(),
 	],
 	markdown: {
