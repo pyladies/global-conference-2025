9e2d667856290c9cdf5fa5329ff98b6a8c1007b5
Isadora <87881791+codingisads@users.noreply.github.com>
2025-12-02 14:41:24 +0100

    adding Lokko Joyce and Wendy N. Sànchez (#109)
    * adding Lokko Joyce and Wendy N. Sànchez
    
    * adding volunteers Ana Caloi and Thaily Samaniego

--- src/data/volunteers.js
+++ src/data/volunteers.js
@@ -22,7 +22,7 @@ const volunteers = [
                 git: "https://github.com/codingisads",
 
             },
-            teams: ["infrastructure", "Program"]
+            teams: ["infrastructure"]
         },
         {
             name: "Tilda Udufo",
@@ -100,6 +100,41 @@ const volunteers = [
             links: {
                 linkedin: "https://www.linkedin.com/in/vadalakiari/",
                 website: "https://medium.com/@vadal_",
+            },
+            teams: ["program"]
+        },
+        {
+            name: "Lokko Joyce Dzifa",
+            image: "/images/volunteers/lokko_joyce.jpg",
+            links: {
+                linkedin: "https://www.linkedin.com/in/joyce-dzifa-lokko/",
+                twitter: "https://x.com/DzifaLokko?s=20",
+            },
+            teams: ["communications"]
+        },
+        {
+            name: "Wendy N. Sànchez",
+            image: "/images/volunteers/wendy_n_sanchez.jpg",
+            links: {
+                linkedin: "https://www.linkedin.com/in/wendy-n-sanchez",
+            },
+            teams: ["communications"]
+        },
+        {
+            name: "Ana C. Zanon",
+            image: "/images/volunteers/ana_caloi.jpg",
+            links: {
+                linkedin: "https://www.linkedin.com/in/anacaloi",
+                instagram: "https://www.instagram.com/anacaloi.data/",
+            },
+            teams: ["program"]
+        },
+        {
+            name: "Thaily Samaniego",
+            image: "/images/volunteers/thaily_samaniego.jpg",
+            links: {
+                linkedin: "https://www.linkedin.com/in/thsamaniego/",
+                instagram: "https://www.instagram.com/thaisamfi/",
             },
             teams: ["program"]
         },
